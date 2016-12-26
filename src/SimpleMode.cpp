cac7f63cf645578560f836fe5da0888d0b92045e
unuldur <julien.defiolles@laposte.net>
2016-12-26 18:46:01 +0100

    Modification du mode de base

--- src/SimpleMode.cpp
+++ src/SimpleMode.cpp
@@ -9,58 +9,17 @@
 #include <iostream>
 using namespace std;
 SimpleMode::SimpleMode() : Mode(2, 10,new TreeUniverse(),"SimpleMode") {
-    universe = new TreeUniverse();
 }
 
 void SimpleMode::setByte(const int x, const int y, const int idJoueur) {
-    int byte = universe->getByte(x,y);
-    universe->setByte(x, y, byte == 0);
-}
-
-string SimpleMode::getDifferenceGeneration(const int gen) {
-    if(gen <= 0)
-    {
-        return "ERROR";
-    }
-    if(gen > nbMaxGenerations)
-    {
-        return "FIN";
-    }
-
-    return "";
-}
-
-int SimpleMode::startRunning() {
-    nbPlayerStart++;
-    if(nbPlayerStart != nbJoueur)
-    {
-        return 0;
-    }
-    cout << "creation thread players" <<endl;
-    for (int i = 0; i < nbMaxGenerations; ++i) {
-    }
-    CreateThread(NULL, 0,SimpleMode::running, this,0,NULL);
-    nbPlayerStart = 0;
-    return 1;
+    int byte = treeUniverse->getByte(x,y);
+    treeUniverse->setByte(x, y, byte == 0);
 }
 
 
 
 int SimpleMode::getWinner() {
     return 0;
-}
-
-DWORD SimpleMode::test()
-{
-    for (int i = 1; i <= nbMaxGenerations; ++i) {
-        universe->runStep();
-    }
-}
-
-DWORD WINAPI SimpleMode::running(void *arg) {
-    cout << "debut thread game" << endl;
-    SimpleMode *Obj = static_cast<SimpleMode*>(arg);
-    return Obj->test();
 }
 
 
--- src/SimpleMode.h
+++ src/SimpleMode.h
@@ -14,19 +14,7 @@ public:
 private:
     virtual void setByte(const int x, const int y, const int idJoueur) override;
 
-    virtual string getDifferenceGeneration(const int gen) override;
-
-    virtual int startRunning() override;
-
     virtual int getWinner() override;
-
-    static DWORD WINAPI running(void *);
-
-    DWORD test();
-
-    TreeUniverse* universe;
-
-    TreeNode*  generations[11];
 };
 
 
